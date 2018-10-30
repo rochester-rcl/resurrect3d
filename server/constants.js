d3beab716dab49fcf5a20fb5a881aa359b95a1b9
jromphf <jromphf@library.rochester.edu>
2018-10-30 14:51:44 -0400

    removed unnecessary server code

--- server/constants.js
+++
@@ -1,2 +0,0 @@
-exports.PRIVATE_KEY = require('./privatekey').key;
-exports.SALT_ROUNDS = 10;
--- server/sample-config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mydb',
-  port: process.env.PORT || 3001
-};
-
-module.exports = config;
