f5b29952ad306dbe11498b2353d5cd48b50daf14
jromphf <jromphf@library.rochester.edu>
2021-06-28 15:25:21 -0400

    add user authentication and serialization functions

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import getEnvVar from "../utils/env";
+import { IVerifyOptions } from "passport-local";
 
 import {
   recordHelper,
@@ -10,6 +11,7 @@ import {
   ErrorResponse
 } from "./helpers";
 import UserModel, { IUserDocument } from "../models/User";
+import { FilterQuery } from "mongoose";
 
 type UserDocumentResponseWithError =
   | DocumentResponse<IUserDocument>
@@ -104,4 +106,107 @@ export async function verifyUser(
     { message: `Could not find an account for token ${token}.` },
     404
   );
+}
+
+export function authenticateClient(
+  req: Request,
+  res: Response
+): Response<{ authenticated: boolean }> {
+  return res.json({ authenticated: req.user !== undefined });
+}
+
+export function authenticateServer(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response<{ authenticated: boolean }> | void {
+  if (req.user === undefined) {
+    return res.status(401).json({
+      authenticaed: false
+    });
+  }
+  return next();
+}
+
+export function logout(
+  req: Request,
+  res: Response
+): Response<void> | ErrorResponse {
+  try {
+    req.logout();
+    return res.json();
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
+  }
+}
+
+export async function userStrategy(
+  email: string,
+  password: string,
+  done: (
+    error?: Error | null,
+    user?: IUserDocument | null,
+    options?: IVerifyOptions
+  ) => void
+): Promise<void> {
+  try {
+    const query = UserModel.findOne({
+      email: email
+    } as FilterQuery<IUserDocument>);
+    const user = await query;
+
+    if (!user) {
+      return done(null, user, {
+        message: `User with email ${email} not found`
+      });
+    }
+
+    if (!user.validPassword(password)) {
+      return done(null, null, {
+        message: `Incorrect password for user ${user.username}`
+      });
+    }
+
+    if (!user.verified) {
+      return done(null, null, {
+        message: `User ${user.username}'s account is not verified`
+      });
+    }
+
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+}
+
+export function serializeUser(
+  user: IUserDocument,
+  done: (
+    error?: Error | null,
+    user?: IUserDocument | null,
+    options?: IVerifyOptions
+  ) => void
+): void {
+  done(null, user._id);
+}
+
+export async function deserializeUser(
+  id: string,
+  done: (
+    error?: Error | null,
+    user?: IUserDocument | null,
+    options?: IVerifyOptions
+  ) => void
+): Promise<void> {
+  try {
+    const query = UserModel.findById(id);
+    const user = await query;
+    if (!user) {
+      throw new Error(`User with id ${id} not found`);
+    }
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
 }
